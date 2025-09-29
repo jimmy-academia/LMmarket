d5f8068c2b9c442f953ba361803ccbda77239a5c
cyyeh <d09942009@ntu.edu.tw>
2025-09-30 06:09:17 +0800

    just another commitment to greatness

--- systems/__init__.py
+++ systems/__init__.py
@@ -1,24 +1,24 @@
-from .method import BestSystem
+# from .method import BestSystem
 from .rm25 import BM25Baseline
-from .ou import OUBaseline
-from .sulm import SULMBaseline
-from .sat import SATBaseline as SegmentAnyTextBaseline
+# from .ou import OUBaseline
+# from .sulm import SULMBaseline
+# from .sat import SATBaseline as SegmentAnyTextBaseline
 
 
 def build_system(args, data):
-    if args.system == 'best':
-        print('[systems] >>> operating OUR PROPOSED METHOD')
-        return BestSystem(args, data)
-    if args.system == 'sat':
-        print('[systems] >>> operating Segment Any Text baseline')
-        return SegmentAnyTextBaseline(args, data)
+    # if args.system == 'best':
+    #     print('[systems] >>> operating OUR PROPOSED METHOD')
+    #     return BestSystem(args, data)
+    # if args.system == 'sat':
+        # print('[systems] >>> operating Segment Any Text baseline')
+        # return SegmentAnyTextBaseline(args, data)
     if args.system == 'bm25':
         print('[systems] >>> operating BM25 retrieval baseline')
         return BM25Baseline(args, data)
-    if args.system == 'ou':
-        print('[systems] >>> operating Opinion Unit baseline')
-        return OUBaseline(args, data)
-    if args.system == 'sulm':
-        print('[systems] >>> operating Sentiment Utility Logistic Model baseline')
-        return SULMBaseline(args, data)
+    # if args.system == 'ou':
+    #     print('[systems] >>> operating Opinion Unit baseline')
+    #     return OUBaseline(args, data)
+    # if args.system == 'sulm':
+    #     print('[systems] >>> operating Sentiment Utility Logistic Model baseline')
+    #     return SULMBaseline(args, data)
     raise ValueError(f"Unknown system '{args.system}'")
