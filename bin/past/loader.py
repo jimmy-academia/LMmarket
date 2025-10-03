f85fcd118cae3611f6ebe4bf469153bba9a054a0
cyyeh <d09942009@ntu.edu.tw>
2025-10-04 03:44:55 +0800

    just another commitment to greatness

--- bin/past/loader.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def get_task_loader(dset):
-    if dset == "yelp":
-        with open("cache/new/benchmark.json", "r") as fp:
-            benchmark = json.load(fp)
-        return benchmark
-    else:
-        raise NotImplementedError(f"task {dset} not implemented")
